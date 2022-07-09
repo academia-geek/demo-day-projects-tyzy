87037e5bf8a3647df79cbc9a377019d404b9f9d4
Marlon Vivas <mj7marlonvivas@gmail.com>
2022-07-08 19:41:13 -0500

    commit initial

---
+++ tyzy/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
