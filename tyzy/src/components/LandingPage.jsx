2e4cdfce7d206a2b9e056b4b2f222cca89799034
francisco bonilla <franciscobonillac@gmail.com>
2022-07-10 23:55:01 -0500

    agregue la seccion dos

--- tyzy/src/components/LandingPage.jsx
+++ tyzy/src/components/LandingPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import LandingPagePartTwo from './LandingPagePartTwo';
 import LandingPageStadist from './LandingPageStadist';
 import LandingPageSup from './LandingPageSup';
 import LandingPageFooter from './LandinPageFooter';
@@ -7,6 +8,7 @@ const LandingPage = () => {
   return (
       <div>
         <LandingPageSup />
+        <LandingPagePartTwo/>
         <LandingPageStadist/>
         <LandingPageFooter />
       </div>
