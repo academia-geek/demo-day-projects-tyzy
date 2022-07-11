ef09d48123643bea8ea2893df5f41a583cd4d453
Marlon Vivas <mj7marlonvivas@gmail.com>
2022-07-11 09:36:07 -0500

    cambios en footer

--- tyzy/src/components/LandinPageFooter.jsx
+++ tyzy/src/components/LandinPageFooter.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom';
-import { FiInstagram, FiYoutube } from "react-icons/fi";
+import { FiInstagram } from "react-icons/fi";
 import { CgFacebook } from "react-icons/cg";
+import { FaYoutube } from "react-icons/fa";
 
 const LandingPageFooter = () => {
   return (
@@ -9,9 +10,9 @@ const LandingPageFooter = () => {
       <img className='IMGFooter my-auto ms-5' src="https://res.cloudinary.com/doaijv8m0/image/upload/v1657514582/Demoday/LogoB_1_mfmniv.png" alt="logo tyzy" />
       <div className='d-flex my-auto text-white fs-6'><p className='my-auto'>Challenge &copy;. Coded by </p><NavLink to='#' className='nav-link my-auto text-white'>Tyzy</NavLink></div>
       <section className='d-flex my-auto me-5 gap-3'>
-        <Link to='#'><FiYoutube color='white' size={20} /></Link>
         <Link to='#'><CgFacebook color='white' size={20} /></Link>
         <Link to='#'><FiInstagram color='white' size={20} /></Link>
+        <Link to='#'><FaYoutube className='' color='white' size={20} /></Link>
       </section>
     </div>
   )
--- tyzy/src/components/LandingPagePartTwo.jsx
+++ tyzy/src/components/LandingPagePartTwo.jsx
@@ -1,32 +1,32 @@
 import React from 'react'
-import perr1 from '../img/imagen1.png'
-import perr2 from '../img/imagen2.png'
-import perr3 from '../img/imagen3.png'
+// import perr1 from '../img/imagen1.png'
+// import perr2 from '../img/imagen2.png'
+// import perr3 from '../img/imagen3.png'
 
 export default function LandingPagePartTwo() {
-  return (
-    <div className='pt-5 px-28' >
-        <section>
-            <h2 className='text-3xl text-titleColor font-bold text-center py-1'>Lorem ipsum dolor sit</h2>
-            <p className='px-20 text-center lg:px-72'>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation, ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in</p>
-        </section>
-        <section className='py-5 px-10 grid gap-3 grid-cols-[350px_1fr_1fr_1fr]'>
-            <div className='flex flex-col justify-center'>
-                <h3 className='text-xl font-bold text-titleColor'>Lorem sit amet </h3>
-                <p className='text-sm text-titleOrange'>LOREM IPSUM</p>
-                <p className='text-sm py-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            </div>
-            <div className='flex justify-center'>
-                <img className='rounded-lg'  src={perr1} alt='perr1'/>
-            </div>
-            <div className='flex justify-center'>
-                <img className='rounded-lg'  src={perr2} alt='perr2'/>
-            </div>
-            <div className='flex justify-center'>
-                <img className='rounded-lg'  src={perr3} alt='perr3'/>
-            </div>
-        </section>
-        
-    </div>
-  )
+    return (
+        <div className='pt-5 px-28' >
+            <section>
+                <h2 className='text-3xl text-titleColor font-bold text-center py-1'>Lorem ipsum dolor sit</h2>
+                <p className='px-20 text-center lg:px-72'>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation, ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in</p>
+            </section>
+            <section className='py-5 px-10 grid gap-3 grid-cols-[350px_1fr_1fr_1fr]'>
+                <div className='flex flex-col justify-center'>
+                    <h3 className='text-xl font-bold text-titleColor'>Lorem sit amet </h3>
+                    <p className='text-sm text-titleOrange'>LOREM IPSUM</p>
+                    <p className='text-sm py-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+                <div className='flex justify-center'>
+                    {/* <img className='rounded-lg' src={perr1} alt='perr1' /> */}
+                </div>
+                <div className='flex justify-center'>
+                    {/* <img className='rounded-lg' src={perr2} alt='perr2' /> */}
+                </div>
+                <div className='flex justify-center'>
+                    {/* <img className='rounded-lg' src={perr3} alt='perr3' /> */}
+                </div>
+            </section>
+
+        </div>
+    )
 }
