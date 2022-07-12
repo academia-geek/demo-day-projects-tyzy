a5f3a0801795c35587f55849db296324f52fb08d
Marlon Vivas <mj7marlonvivas@gmail.com>
2022-07-11 20:02:03 -0500

    styles de landing services

--- tyzy/src/components/LandinPageFooter.jsx
+++ tyzy/src/components/LandinPageFooter.jsx
@@ -9,6 +9,7 @@ const LandingPageFooter = () => {
     <div id='footer' className='LandingPageFooter d-flex justify-content-between'>
       <img className='IMGFooter my-auto ms-5' src="https://res.cloudinary.com/doaijv8m0/image/upload/v1657514582/Demoday/LogoB_1_mfmniv.png" alt="logo tyzy" />
       <div className='d-flex my-auto text-white fs-6'><p className='my-auto'>Challenge &copy;. Coded by </p><NavLink to='#' className='link my-auto text-white'>Tyzy</NavLink></div>
+      
       <section className='d-flex my-auto me-5 gap-3'>
         <Link to='#'><CgFacebook color='white' size={20} /></Link>
         <Link to='#'><FiInstagram color='white' size={20} /></Link>
