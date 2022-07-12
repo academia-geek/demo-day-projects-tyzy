d3b591b6a222a55b5675de504fbabf2fba7d65be
francisco bonilla <franciscobonillac@gmail.com>
2022-07-11 19:48:09 -0500

    solucion de problema

--- tyzy/src/components/LandingPagePreFooter.jsx
+++ tyzy/src/components/LandingPagePreFooter.jsx
@@ -8,24 +8,28 @@ export default function LandingPagePreFooter() {
         <p className=' font-bold text-sm  text-center text-titleOrange'>LOREM IPSUM</p>
     </section>
     <section className='flex justify-center gap-5 py-20 '>
+    
         <div className='flex flex-col items-center'>
             <img src='https://res.cloudinary.com/dg29vcpk7/image/upload/v1657548631/Tyzy/Images_ktxpb8.png' alt='personas felices'  />
             <div className='flex flex-row items-center gap-2 px-4 pt-2'>
            <div className='border-t-2 w-7 border-red-300'></div> <p className='font-bold text-2xl block'>Personas más felices y tranquilas</p>
            </div>
         </div>
+
         <div  className='flex flex-col items-center'>
             <img src='https://res.cloudinary.com/dg29vcpk7/image/upload/v1657548631/Tyzy/Images-1_doemkk.png' alt='personas felices'  />
             <div className='flex flex-row items-center gap-2  px-4   pt-2'>
            <div className='border-t-2 w-7 border-red-300'></div> <p className='font-bold text-2xl block'>Tenencia y manutención tranquila</p>
            </div>
         </div>
+
         <div className='flex flex-col items-center'>
             <img src='https://res.cloudinary.com/dg29vcpk7/image/upload/v1657548631/Tyzy/Images-2_eokshd.png' alt='personas felices'  />
            <div className='flex flex-row items-center gap-2  px-4  pt-2'>
            <div className='border-t-2 w-7 border-red-300'></div> <p className='font-bold text-2xl block'>Confianza sin importar la edad</p>
            </div>
         </div>
+
     </section>
     </div>
   )
