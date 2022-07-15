fb7a14099ab05130f8b6d64cab653ae2975f6451
francisco bonilla <franciscobonillac@gmail.com>
2022-07-14 22:53:04 -0500

    configurando el boton para enditar

--- tyzy/src/components/Perfil.jsx
+++ tyzy/src/components/Perfil.jsx
@@ -1,25 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NavBarIn from './NavBarIn'
 import { BsFillCameraFill } from "react-icons/bs";
+import { getAuth } from 'firebase/auth';
 
 export default function Perfil() {
+  const [usuario, setUsuario] = useState(null)
+  useEffect(() => {
+    const auth = getAuth();
+    const dUsuarios = auth.currentUser;
+    const CargarPhoto = async (dataUser) => {
+      return await dataUser;
+    };
+    CargarPhoto(dUsuarios)
+      .then((user) => {
+        user ? setUsuario(dUsuarios) : console.log('no se pudo', user);
+      })
+      .catch(err => console.log('no se pudo cargar la imgen', err))
+  }, [])
+
   return (
     <div>
     <NavBarIn/>
-    <div className='p-20 bg-bgBlanco'>
+    <div className='py-20 px-5 flex flex-col items-center bg-bgBlanco'>
       <section className='mb-10'>
         <h3 className='text-titleOrange font-bold text-2xl'>Tu perfil</h3>
       </section>
 
       <section className='flex flex- items-center'>
-      <div>
+      <div className='flex flex-col'>
         <img src='https://res.cloudinary.com/dg29vcpk7/image/upload/v1657548600/Tyzy/Image-2_svuu7t.png' alt=''/>
-        <p className='text-center text-titleColor py-3'><BsFillCameraFill className='inline-block text-xl'/> Cambiar foto de perfil</p>
+        <button className='flex gap-2 text-titleColor  py-3'><BsFillCameraFill className='inline-block text-xl'/> Cambiar foto de perfil</button>
       </div>
       <div >
       <form className='grid grid-cols-[1fr_300px] gap-x-10 gap-y-2 px-5 items-center'>
         <label className='inline' htmlFor='nombre'>Nombres:</label>
-        <input className=' appearance-none border-0 rounded-xl w-full  text-gray-700 leading-tight focus:outline-none focus:shadow-outline'  type='text' name='nombre' placeholder='Francisco' />
+        <input className=' appearance-none border-0 rounded-xl w-full  text-gray-700 leading-tight focus:outline-none focus:shadow-outline'  type='text' name='nombre' placeholder={usuario.displayName} />
 
         <label>Apellidos:</label>
         <input className=' appearance-none border-0 rounded-xl w-full  text-gray-700 leading-tight focus:outline-none focus:shadow-outline' type='email' name='email' placeholder='Bonilla' />
