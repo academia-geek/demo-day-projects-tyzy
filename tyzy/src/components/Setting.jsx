edbb495eba5276038a35b2373a29e61276d8bc2b
Marlon Vivas <mj7marlonvivas@gmail.com>
2022-07-19 20:25:29 -0500

    correciones

---
+++ tyzy/src/components/Setting.jsx
@@ -0,0 +1,78 @@
+import { getAuth } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
+import { Nav, Offcanvas } from 'react-bootstrap';
+import { RiUserFill } from "react-icons/ri";
+import { IoMdNotifications } from "react-icons/io";
+import { BsQuestionLg } from "react-icons/bs";
+import { MdSettings, MdClose } from "react-icons/md";
+import { TbLogout } from "react-icons/tb";
+import { ActionLogoutAsync } from '../redux/actions/LoginActions';
+import { useDispatch } from 'react-redux';
+import { Link, NavLink } from 'react-router-dom';
+import { Avatar } from 'flowbite-react';
+import { NavDesplegable } from '../styles/StylesGlobals';
+
+const Setting = ({ name, ...props }) => {
+    const dispatch = useDispatch()
+    // ----------------------------------------------------------
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    // --------------------------------------------------------------
+    const [usuario, setUsuario] = useState(null)
+
+    useEffect(() => {
+        const auth = getAuth();
+        const dUsuarios = auth.currentUser;
+        setUsuario(dUsuarios)
+    }, [])
+
+    return (
+        <NavDesplegable>
+            <div className='my-auto d-flex'><h2 className='fs-3'>{usuario?.displayName}</h2><img onClick={handleShow} className='my-auto ms-2' role='button' src="https://res.cloudinary.com/doaijv8m0/image/upload/v1658024388/Demoday/Vector_5_plaaek.png" alt="" /></div>
+
+            <Offcanvas className='NavD' show={show} onHide={handleClose} {...props}>
+                <Offcanvas.Header >
+                    <Offcanvas.Title className='d-flex mt-4 ms-4 w-100'>
+                        <div className='d-flex my-auto'>
+                            <Avatar className='fs-6 my-auto' alt={usuario?.displayName} img={usuario?.photoURL} rounded={true} />
+                            <h2 className='ms-2 fs-4 text-white my-auto'>{usuario?.displayName}</h2>
+                        </div>
+
+                        <MdClose role='button' onClick={handleClose} className='text-white my-auto ms-auto me-5' />
+                    </Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    <Nav className="mx-auto d-flex flex-column ">
+                        <div className='d-flex ms-3 me-5'>
+                            <NavLink className='nav-link text-white' to="/perfil"><h4 className=''>Perfil</h4></NavLink>
+                            <Link className='my-auto ms-auto' to='/perfil'><RiUserFill color='#B2D9D1' /></Link>
+                        </div>
+                        <div className='d-flex ms-3 me-5'>
+                            <NavLink className='nav-link text-white' to="#Detail"><h4 className=''>Notificaciones</h4></NavLink>
+                            <Link className='my-auto ms-auto' to='#'><IoMdNotifications color='#B2D9D1' /></Link>
+                        </div>
+                        <div className='d-flex ms-3 me-5'>
+                            <NavLink className='nav-link text-white' to="#fr3"><h4 className=''>Preguntas PQR</h4></NavLink>
+                            <Link className='my-auto ms-auto' to='#'><BsQuestionLg color='#B2D9D1' /></Link>
+                        </div>
+                        <div className='d-flex ms-3 me-5'>
+                            <NavLink className='nav-link text-white' to="#Footer"><h4 className=''>Configuración</h4></NavLink>
+                            <Link className='my-auto ms-auto' to='#'><MdSettings color='#B2D9D1' /></Link>
+                        </div>
+                        <div className='d-flex ms-3 me-5 mt-2'>
+                            <div role='button' className='my-auto ms-3' onClick={() => dispatch(ActionLogoutAsync())}>
+                                <h2 className='text-white'>Cerrar sesión</h2>
+                            </div>
+                            <Link className='my-auto ms-auto' to='#'><TbLogout color='#B2D9D1' /></Link>
+                        </div>
+                    </Nav>
+                </Offcanvas.Body>
+            </Offcanvas>
+        </NavDesplegable>
+    )
+}
+
+export default Setting;
