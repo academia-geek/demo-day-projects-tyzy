9b92354e415edf3ccc8d9b6cc5326662b0166337
Marlon Vivas <mj7marlonvivas@gmail.com>
2022-07-24 12:42:17 -0500

    pull de main

--- tyzy/src/components/Comunidad/ComunidadList.jsx
+++ tyzy/src/components/Comunidad/ComunidadList.jsx
@@ -3,24 +3,26 @@ import { useDispatch, useSelector } from 'react-redux'
 import { listaComuniAsync } from '../../redux/actions/ActionAddComuni'
 
 export default function ComunidadList() {
-    const dispatch = useDispatch()
-    const { comunidad } = useSelector(store => store.comunidades)
-    console.log(comunidad);
-    useEffect(() => {
-        dispatch(listaComuniAsync())
-    }, [dispatch])
+
+  const dispatch = useDispatch()
+  const { comunidad } = useSelector(store => store.comunidades)
+  console.log(comunidad);
+
+  useEffect(() => {
+    dispatch(listaComuniAsync())
+  }, [dispatch])
+
   return (
     <div className='py-10'>ComunidadList
-     {comunidad.map((p, idx) => (
-            <div key={idx}>
-                <img src={p.imagen} alt={p.descripcion} />
-                <p>{p.descripcion}</p>
-                <p>{p.direccion}</p>
-            </div>
-        ))}
-    <div>
-       
-    </div>
+      {
+      comunidad?.map((p, idx) => (
+        <div key={idx}>
+          <img src={p.imagen} alt={idx} />
+          <p>{p.descripcion}</p>
+          <p>{p.direccion}</p>
+        </div>
+      ))
+      }
     </div>
   )
 }
